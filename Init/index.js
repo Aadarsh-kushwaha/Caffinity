fc816499ec7b7f5991467db6b37927c8072b51af
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2026-07-04 06:54:51 +0530

    Remove hardcoded MongoDB URI

--- Init/index.js
+++ Init/index.js
@@ -7,11 +7,8 @@ const mongoose = require('mongoose');
 const Product = require('../models/product.js'); // correct relative path
 const initData = require("./init.js");
 
-// ====== MongoDB Connection ======
-//  const dburl = "mongodb://127.0.0.1:27017/coffee-shop";
 
 
-// const mongoURL = "mongodb+srv://Aadarsh_kushwaha:AadarshAadarsh@cluster0.3nfifrj.mongodb.net/?appName=Cluster0"; 
 
 const mongoURL = process.env.ATLASDB_URL ;
 
