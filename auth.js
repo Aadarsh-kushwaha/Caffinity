ad10fd6e7658a5087069db66cbc555703a6a5a68
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2026-07-04 17:08:41 +0530

    calback link set properly

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/google/callback",
+            callbackURL: "https://daily-drip-sable.vercel.app/google/callback",
         },
 
         async (accessToken, refreshToken, profile, done) => {
